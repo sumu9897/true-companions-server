4fbffbf587760eee2dc100a9e8fdcc569c80531c
sumu9897 <sumon.bncd@gmail.com>
2025-01-17 18:07:54 +0600

    User Data Store in database

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmec6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,11 +25,24 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    // Define collections
+    const userCollection = client.db("trueCompanions").collection("users");
+    const biodataCollection = client.db("trueCompanions").collection("biodatas");
+    const contactRequestCollection = client.db("trueCompanions").collection("contactRequests");
+    const successStoryCollection = client.db("trueCompanions").collection("successStories");
+
+    //Users Related API 
+    app.post('/users', async (req, res) =>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -38,11 +50,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Search You Partner')
-})
+app.get("/", (req, res) => {
+  res.send("Search You Partner");
+});
 
 app.listen(port, () => {
-    console.log(`True Companions Server is Running ${port}`);
-})
+  console.log(`True Companions Server is Running ${port}`);
+});
