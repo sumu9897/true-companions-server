a6e2cc7a83e39651bd1ab32e1593e53acc32074f
sumu9897 <sumon.bncd@gmail.com>
2025-01-18 09:32:47 +0600

    User role update and Delete User

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmec6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,10 +32,39 @@ async function run() {
     const successStoryCollection = client.db("trueCompanions").collection("successStories");
 
     //Users Related API 
+    app.get('/users', async (req,res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
     app.post('/users', async (req, res) =>{
       const user = req.body;
+
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'User already exists', insertedId: null})
+      }
       const result = await userCollection.insertOne(user);
-      res.send(result)
+      res.send(result);
+    })
+    //make admin api
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      } 
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    } )
+    //user deleted api
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query ={ _id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
