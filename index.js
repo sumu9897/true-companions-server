82ca312b69de91c70752258e694aafd4d91264ea
sumu9897 <sumon.bncd@gmail.com>
2025-01-18 10:41:35 +0600

    JWT token set

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken')
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -31,11 +32,67 @@ async function run() {
     const contactRequestCollection = client.db("trueCompanions").collection("contactRequests");
     const successStoryCollection = client.db("trueCompanions").collection("successStories");
 
+    // JWT related API
+    app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({token});
+    })
+
+    // middlewares
+    // Verify Token
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token',req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'unatorized access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'unatorized access'})
+        }
+        req.decoded= decoded;
+        next();
+      })
+      // next()
+    }
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async(req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next();
+
+    }
+
     //Users Related API 
-    app.get('/users', async (req,res) => {
+    app.get('/users',verifyToken, verifyAdmin, async (req,res) => {
       const result = await userCollection.find().toArray();
       res.send(result)
     })
+    app.get('/users/admin/:email',verifyToken, async(req, res) =>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    } )
+
+
     app.post('/users', async (req, res) =>{
       const user = req.body;
 
@@ -48,7 +105,7 @@ async function run() {
       res.send(result);
     })
     //make admin api
-    app.patch('/users/admin/:id', async(req, res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async(req, res) => {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
       const updatedDoc = {
@@ -60,7 +117,7 @@ async function run() {
       res.send(result);
     } )
     //user deleted api
-    app.delete('/users/:id', async(req, res) => {
+    app.delete('/users/:id', verifyToken, verifyAdmin, async(req, res) => {
       const id = req.params.id;
       const query ={ _id: new ObjectId(id)}
       const result = await userCollection.deleteOne(query);
